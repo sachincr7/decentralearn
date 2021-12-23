4b34ea0001ad8029efdca23c0f7dc32f980fe72f
sachin00777 <sachinaugustine11@gmail.com>
2021-12-24 04:05:00 +0530

    main and marketplace ui created

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -1,4 +1,4 @@
 export { default as Navbar } from "./navbar";
 // export { default as Footer } from "./footer";
-export { default as Button } from "./button";
+export { default as Button } from "./Button";
 // export { default as Footer } from "./footer";
