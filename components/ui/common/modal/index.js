479462837f693f09a56dc9325794d8b241b9e567
sachin00777 <sachinaugustine11@gmail.com>
2021-12-25 18:46:58 +0530

    text color input changed to black

--- components/ui/common/modal/index.js
+++ components/ui/common/modal/index.js
@@ -55,7 +55,7 @@ export default function Modal({ selectedCourse, setSelectedCourse }) {
                       aria-hidden="true"
                     />
                   </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <div className="mt-3  sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title
                       as="h3"
                       className="text-lg leading-6 font-medium text-gray-900"
